a361cc7778004dea4f4a41ee9162831e003c9a71
Vojta <favox000@gmail.com>
2025-06-28 08:01:06 +0200

    přidán gitignore a napojena databáze na webu neon

--- buildthetown/buildthetown/settings.py
+++ buildthetown/buildthetown/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,12 +73,12 @@ WSGI_APPLICATION = 'buildthetown.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
